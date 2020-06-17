08330f91d56bbe94959cf82b24263f7a916223ce
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-06-17 16:05:12 +0200

    added Firebase to my app

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as firebase from 'firebase' 
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'; 
 import '@fortawesome/fontawesome-free/css/all.css'
 import { store } from '@/store/state'
 
@@ -11,5 +12,14 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBTEJYsbaqn2ZhR15uOv8jOmtVMtD-7Hj4",
+      authDomain: "hotel-booking-10a12.firebaseapp.com",
+      databaseURL: "https://hotel-booking-10a12.firebaseio.com",
+      projectId: "hotel-booking-10a12",
+      storageBucket: "hotel-booking-10a12.appspot.com"
+    })
+  }
 }).$mount('#app')
