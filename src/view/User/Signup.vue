224b8d0d4c36c217101023dcbe619c5f62080b0a
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-06-17 16:22:20 +0200

    Started with the Signup view

--- src/view/User/Signup.vue
+++ src/view/User/Signup.vue
@@ -1,11 +1,60 @@
 <template>
-    <div>
-        <p>Signup</p>
-    </div>
+    <v-container>
+        <v-form>
+            <v-layout column class="pa-2">
+
+                <v-flex xs12>
+                    <v-text-field
+                        name="name"
+                        label="Your Name"
+                        id="name"
+                        v-model="userName"
+                    ></v-text-field>
+                </v-flex>
+
+                <v-flex xs12>
+                    <v-text-field
+                        name="email"
+                        label="Email adress"
+                        id="email"
+                        v-model="userEmail"
+                        type="email"
+                    ></v-text-field>
+                </v-flex>
+
+                <v-flex xs12>
+                    <v-text-field
+                        name="password"
+                        label="Password"
+                        id="password"
+                        v-model="userPassword"
+                        type="password"
+                    ></v-text-field>
+                </v-flex>
+
+                <v-flex xs12>
+                    <v-btn type="submit">Sign up</v-btn>
+                </v-flex>
+
+            </v-layout>
+        </v-form>
+    </v-container>
 </template>
 
 <script>
 export default {
-    name: 'signup'
+    data () {
+        return {
+            userName: '',
+            userEmail: '',
+            userPassword: ''
+        }
+    },
+
+    methods: {
+        onSignUp () {
+            
+        }
+    }
 }
 </script>
