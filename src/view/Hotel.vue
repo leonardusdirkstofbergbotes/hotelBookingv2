76f59889ce7e3beb2798a311fa0eb2a398727e70
Leonardus <leonardusdirkstofbergbotes@gmail.com>
2020-06-22 18:12:41 +0200

    styled the book dialog

--- src/view/Hotel.vue
+++ src/view/Hotel.vue
@@ -21,17 +21,120 @@
                 {{hotel.info}}
             </v-flex>
 
-            <v-layout column v-for="item in bookings" :key='item.adults'>
-                <v-flex>
-                     Adults: R {{item.adults * (days * hotel.price)}}
+            
+
+            <v-dialog
+      v-model="book"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn v-bind="attrs" v-on="on">Book</v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title class="headline blue lighten-4 mb-4" primary-title>
+          Review your Booking information
+        </v-card-title>
+
+        <h2 class="display-2 text-center">{{hotel.name}}</h2>
+        <v-img max-height="250px" class="mx-4 rounded-t-circle" :src="hotel.imageSrc"></v-img>
+        
+
+        <v-layout column v-for="item in bookings" :key='item.adults' class="mx-4 pa-2">
+            <v-layout row class="my-6">
+                <v-layout column>
+                    <v-flex class="text-center">
+                        <v-icon left>fa-calendar-check</v-icon>
+                        <b>Check in date</b>
+                    </v-flex>
+                    <v-flex class="text-center">
+                        {{item.dateIn}}
+                    </v-flex>
+                </v-layout>
+
+                <v-layout column>
+                    <v-flex class="text-center">
+                        {{days}}
+                    </v-flex>
+                    <v-flex class="text-center">
+                        days
+                    </v-flex>
+                </v-layout>
+
+                <v-layout column>
+                    <v-flex class="text-center">
+                        <b>Check out date</b>
+                        <v-icon right>fa-calendar-minus</v-icon>
+                    </v-flex>
+                    <v-flex class="text-center">
+                        {{item.dateOut}}
+                    </v-flex>
+                </v-layout>
+            </v-layout>
+
+
+            <v-layout row align-center>
+                <v-flex shrink>
+                    Adults: R {{item.adults * (days * hotel.price)}}  
                 </v-flex>
-                <v-flex v-if="item.children !== 0">
-                    Children: R {{item.children * (hotel.price/2 * days)}} (50% off)
-                </v-flex>
-                <v-flex>
-                    <b>Total: R {{ (item.adults * (days * hotel.price)) + (item.children * (hotel.price/2 * days)) }}</b>
+                
+                <v-flex shrink>
+                    <v-rating xs12 md6 lg6
+                        :value="item.adults"
+                        :length="item.adults"
+                        empty-icon=""
+                        full-icon="fa-male"
+                        readonly
+                        background-color="grey lighten-2"
+                        color="orange accent-3"
+                        size="20"
+                    ></v-rating>
                 </v-flex>
             </v-layout>
+
+            <v-layout row align-center v-if="item.children !== 0" class="mb-7">
+                <v-flex  shrink>
+                    Children: R {{item.children * (hotel.price/2 * days)}} <i class="grey--text customText">(50% off)</i>
+                </v-flex>
+
+                <v-flex shrink>
+                    <v-rating xs12 md6 lg6
+                        :value="item.children"
+                        :length="item.children"
+                        empty-icon=""
+                        full-icon="fa-child"
+                        readonly
+                        background-color="grey lighten-2"
+                        color="orange accent-3"
+                        size="20"
+                    ></v-rating>
+                </v-flex>
+            </v-layout>
+
+
+            
+            <v-flex class="ml-auto red--text">
+                <b class="text-decoration-underline">Total: R {{ (item.adults * (days * hotel.price)) + (item.children * (hotel.price/2 * days)) }} <i class="grey--text customText">incl VAT</i></b>
+            </v-flex>
+
+        </v-layout>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="red"
+            dark
+            @click="book = false"
+          >
+            Proceed to checkout
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+            
         </v-layout>
     </v-container>
 </template>
@@ -39,6 +142,13 @@
 <script>
 export default {
     props: ['id'],
+
+    data () {
+        return {
+            book: false
+        }
+    },
+
     computed: {
 
         bookings () {
@@ -55,3 +165,10 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.customText {
+    font-size: 10px!important ;
+}
+
+</style>
